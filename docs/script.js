b6cea4810bfa5639ddc5146ffdbc1d2ffa8afe02
maroon <hideo_kurita@yahoo.co.jp>
2024-01-06 22:41:56 +0900

    initial import

---
+++ docs/script.js
@@ -0,0 +1,10 @@
+let count = 0
+document.querySelector('#load').addEventListener('click', async (event) => {
+  const jsonname = document.querySelector('#loadjson').value
+  console.log(jsonname)
+  const response = await fetch(jsonname)
+  const json = await response.json()
+  console.log(json)
+  document.querySelector('#name').innerHTML = json.name
+  document.querySelector('#age').innerHTML = json.age
+})
